30d787ea0ed0a5fefaf80db29a8d584a7f98f0c1
aditkalyani <aditkalyani1111@gmail.com>
2021-05-18 12:07:36 +0530

    Added patch and delete endpoints

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ const port = process.env.port || 3000
 app.use(express.json()) //parses incoming json request to object
 
 //******creating user
-app.post('/users', (req, res)=>{
+app.post('/users', async (req, res)=>{
     const user = new User(req.body)
-    
-    user.save().then(()=>{
-        console.log(user)
+
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e)=>{
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
 //******creating task
@@ -73,6 +73,91 @@ app.get('/tasks/:id', (req, res)=>{
     }).catch(()=>{
         res.status(404).send()
     })
+})
+
+app.patch('/users/:id', async (req,res)=>{
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["name", "email", "password", "age"]
+
+    const isValidOption = updates.every((update)=>{
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOption){
+        return res.status(400).send("Error: No such field")
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch(e){
+        res.status(400).send(e)
+    } 
+})
+
+app.patch('/tasks/:id', async (req,res)=>{
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidOption = updates.every(update => allowedUpdates.includes(update)) 
+
+    if(!isValidOption){
+        return res.status(400).send("Error: Not a valid field")
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators:true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send()  
+        }
+
+        res.send(user)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+    
+})
+
+app.delete('/tasks/:id', async (req,res)=>{
+    
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+
 })
 
 app.listen(port, ()=>{
